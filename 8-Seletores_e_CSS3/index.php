11ab52b16d2c1a5dc4ed9805f0f483e84171e7a4
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-03-29 19:15:42 -0300

    Curso HTML e CSS

---
+++ 8-Seletores_e_CSS3/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction:column;
+        }
+        body a{
+            padding-bottom:5px;
+        }
+    </style>
+</head>
+<body>
+    <a href="1-Seletor_pelo_tributo.html">1 Seletar pelo atributo</a>
+    <a href="2-Seletor_pelo_tributo.html">2 Seletar pelo atributo</a>
+    <a href="3-Seletor_Elemento_Vazio.html">3-Seleter de Elemento Vazio</a>
+    <a href="4-Seletor_Primeira_letra_Primeira_Linha.html">4-Seletor_Primeira_letra_Primeira_Linha</a>
+    <a href="5-Seletor_Primeiro_Último_Elemento.html">5-Seleter de Primeira e Último Elemento</a>
+    <a href="6-SeletorNegativo.html">6-Seletar Negativo</a>
+    <a href="7-Seletor-NTH.html">7-Seletar NTH</a>
+</body>
+</html>
